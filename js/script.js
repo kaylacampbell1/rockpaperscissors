30c2a01741ad10706422fc42623e6355a9f0b21c
Kayla Campbell <campbellkayla2003@gmail.com>
2018-11-28 22:54:51 +0000

    rock, paper, scissors

--- js/script.js
+++ js/script.js
@@ -8,16 +8,34 @@
 
 
 // DOCUMENT READY FUNCTION BELOW
-var userChoice =- "";
+var userChoice =$("#input").val();
 var computerChoice = "";
 var winner = "";
-var randomNumber = 0;
+var randomNumber = Math.random();
 
+if(randomNumber===1){
+    computerChoice = "rock"
+}
+if(randomNumber===2){
+    computerChoice = "paper"
+}
+if(randomNumber===3){
+    computerChoice = "scissor"
+}
 
 $("#shoot").click(function(){
-    var us = $("#input").val();
-   $("#userChoice").text(us);
-   randomNumber = Math.random();
+    if (userChoice===computerChoice){
+    $("#result").html("TIE!");
+    }
+    if ((userChoice==="rock" && computerChoice==="scissor")||(userChoice==="paper"&&computerChoice==="rock")||(userChoice==="scissor"&&computerChoice==="paper")){
+    $("#result").html("You Win!");
+    }
+    if ((computerChoice==="rock" && userChoice==="scissor")||(computerChoice==="paper"&&userChoice==="rock")||(computerChoice==="scissor"&&userChoice==="paper")){
+    $("#result").html("Computer Win!");
+    }
+   $("#userChoice").html(userChoice);
+   $("#computerChoice").html(computerChoice);
+   
 });
 
 
